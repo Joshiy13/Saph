dded1fe9cb786f56eeb4bc833efbefa1f37e2f30
Joshiy13 <weidanzjoshua@gmail.com>
2022-12-04 15:17:23 +0100

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+client = commands.Bot()
+token = os.getenv("TOKEN")
+
+
+@client.event
+async def on_ready():
+    client.loop.create_task(status_task())
+    print("Bot is ready!")
+
+
+
+
+
+#Fügt den Status des Bots hinzu
+async def status_task():
+    while True:
+        await client.change_presence(activity=discord.Game(name="/help"))
+        await asyncio.sleep(10)
+        await client.change_presence(activity=discord.Game(name="Coded by @joshiy13#7277"))
+        await asyncio.sleep(10)
+
+
+
+
+
+#Fügt einen Slash command hinzu
+@client.slash_command(name="test", description="Tests if the Bot is running", guild_ids=[960188361874747393]) # test server guild id must be changed before deployment
+async def first_slash(ctx): 
+    await ctx.respond("Test completed!")
+
+
+
+
+
+client.run(token) #runnt den bot
